daa1e9adfc448c1ad1a4a70ba56cda06940b3af5
Rohan Date <rohan.s.date@icloud.com>
2025-02-01 15:18:24 -0800

    Added error return type

--- build.go
+++ build.go
@@ -1,6 +1,8 @@
 package rpkgengine
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -13,54 +15,60 @@ type RpkgBuildFile struct {
 	Version       string   `yaml:"version"`
 	Revision      string   `yaml:"revision"`
 	Authors       []string `yaml:"authors"`
+	Deps          []string `yaml:"deps"`
+	BuildDeps     []string `yaml:"build_deps"`
 	BuildWith     string   `yaml:"build_with"`
 	BuildCommands []string `yaml:"build_commands"`
 }
 
-func unmarshall(filename string) (int, RpkgBuildFile) {
+func unmarshall(filename string) (int, RpkgBuildFile, error) {
 	// Empty struct for return purposes
 	var g RpkgBuildFile
 	// Read the file
 	f, err := os.ReadFile(filename)
 	if err != nil {
-		return 1, g
+		return 1, g, errors.New("wasn't able to read file")
 	}
 	// Create the RpkgBuildFile and interface variables (for unmarshalling)
 	var F RpkgBuildFile
 	var raw interface{}
 	// Unmarshall the YAML
 	if err := yaml.Unmarshal(f, &raw); err != nil {
-		return 1, g
+		return 1, g, errors.New("YAML unmarshalling failed")
 	}
 	// Decode the unmarshalled YAML using mapstructure (shortened to mpstruct)
 	decoder, _ := mpstruct.NewDecoder(&mpstruct.DecoderConfig{WeaklyTypedInput: true, Result: &F})
 	if err := decoder.Decode(raw); err != nil {
-		return 1, g
+		return 1, g, errors.New("YAML decoding failed")
 	}
-	return 0, F
+	return 0, F, nil
 }
 
-func build() (int, string) {
-	code, f := unmarshall("rpkg.build.yaml")
-	if code == 1 {
-		return 1, "YAML unmarshalling failed."
+func build() (int, error) {
+	code, f, err := unmarshall("./rpkg.build.yaml")
+	if code == 1 && err != nil {
+		return 1, errors.New("YAML unmarshalling failed")
 	}
 	switch lang := f.BuildWith; lang {
 	case "python3.13":
+		fmt.Print("Finding Python... ")
 		cmd := exec.Command("python3.13", "-v")
 		if _, err := cmd.Output(); err != nil {
-			return 1, "Python 3.13 was not found on your system."
+			return 1, errors.New("python 3.13 was not found on your system")
 		} else {
-			cmds := ""
-			for i := 0; i < len(f.BuildCommands); i++ {
-				cmds = cmds + f.BuildCommands[i] + " && "
-			}
-			cmds = cmds[:len(cmds)-4]
-			cmd := exec.Command(cmds)
-			if _, err := cmd.Output(); err != nil {
-				return 1, "Build commands could not be run"
-			}
+			fmt.Print("Found version 3.13")
+			fmt.Println("Installing dependencies...")
+		}
+
+		cmds := ""
+		for i := 0; i < len(f.BuildCommands); i++ {
+			cmds = cmds + f.BuildCommands[i] + " && "
+		}
+		cmds = cmds[:len(cmds)-4]
+		Cmd := exec.Command(cmds)
+		if _, err := Cmd.Output(); err != nil {
+			return 1, errors.New("build commands could not be run")
 		}
 	}
-	return 0, "Package built successfully"
+	return 0, nil
 }
