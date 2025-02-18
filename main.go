b6e9617d113e64174ca7a97059c36ddba566a69d
Rohan Date <rohan.s.date@icloud.com>
2025-02-18 13:31:12 -0800

    Updated build function to include a new parameter for enhanced functionality

--- main.go
+++ main.go
@@ -20,5 +20,5 @@ func main() {
 	f.Name = viper_instance.Get("name").(string)
 	f.Revision = viper_instance.Get("revision").(int)
 	f.Version = viper_instance.Get("version").(string)
-	re.Build("../rpkg-test", f)
+	re.Build("../rpkg-test", f, true)
 }
