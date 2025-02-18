70c0a972ff6ea81c0f7d02f592880fc81be512be
Rohan Date <rohan.s.date@icloud.com>
2025-02-18 12:36:46 -0800

    Moved all the dependency installation code to rpkgengine.go

--- rpkgengine/build.go
+++ rpkgengine/build.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 func Build(project string, f RpkgBuildFile) (int, error) {
 	os.Chdir(project + "/Package")
 	switch lang := f.BuildWith; lang {
 	case "python3.13":
-		fmt.Printf("Creating new venv for %s...", []any{project}...)
+		fmt.Printf("Creating new venv for %s...\n", []any{project}...)
 		c := exec.Command("python3.13", "-m", "venv", project)
 		c.Stdout = nil
 		if _, err := c.Output(); err != nil {
 			return 1, errors.New("could not create venv")
 		}
+		fmt.Println("Venv created")
 		fmt.Print("Scanning for Python... ")
 		cmd := exec.Command("python3.13", "-v")
 		cmd.Stdout = nil
@@ -25,68 +25,16 @@ func Build(project string, f RpkgBuildFile) (int, error) {
 			fmt.Print("Not Found")
 			return 1, errors.New("python 3.13 was not found on your system")
 		} else {
-			fmt.Print("Found version 3.13")
+			fmt.Print("Found version 3.13\n")
 			fmt.Println("Installing build dependencies... ")
 		}
-		for i := 0; i < len(f.BuildDeps); i++ {
-			if len(f.BuildDeps) == 0 {
-				fmt.Println("No build dependencies")
-				break
-			} else if dep, ok := f.BuildDeps[i].(string); ok && !strings.Contains(dep, "@") {
-				return 1, fmt.Errorf("build dependency %s is not a valid dependency", dep)
-			} else if dep, ok := f.BuildDeps[i].(string); ok && strings.Contains(dep, "@latest") {
-				fmt.Printf("Installing %s... ", []any{f.BuildDeps[i]}...)
-				dep := strings.Split(f.BuildDeps[i].(string), "@")[0]
-				cmd := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", dep)
-				cmd.Stdout = nil
-				if _, err := cmd.Output(); err != nil {
-					fmt.Printf("Could not install %s\n", []any{dep}...)
-					return 1, fmt.Errorf("could not install dependency %s", dep)
-				} else {
-					fmt.Printf("Installed %s\n", []any{dep}...)
-				}
-			} else {
-				dep := strings.Split(f.BuildDeps[i].(string), "@")
-				cmd := exec.Command("python3.13", "-m", "pip", "install", dep[0], "==", dep[1])
-				cmd.Stdout = nil
-				if _, err := cmd.Output(); err != nil {
-					fmt.Printf("Could not install %s\n", []any{dep}...)
-					return 1, fmt.Errorf("could not install dependency %s", dep)
-				} else {
-					fmt.Printf("Installed %s\n", []any{dep[0]}...)
-				}
-			}
+		if _, err := installDeps(f.BuildDeps, true); err != nil {
+			return 1, err
 		}
 		fmt.Println("Build dependencies installed")
 		fmt.Println("Installing dependencies... ")
-		for i := 0; i < len(f.Deps); i++ {
-			if f.Deps[0] == "none" {
-				fmt.Println("No dependencies")
-				break
-			} else if dep, ok := f.Deps[i].(string); ok && !strings.Contains(dep, "@") {
-				return 1, fmt.Errorf("dependency %s is not a valid dependency", dep)
-			} else if dep, ok := f.Deps[i].(string); ok && strings.Contains(dep, "@latest") {
-				fmt.Printf("Installing %s... ", []any{f.Deps[i]}...)
-				dep := strings.Split(f.Deps[i].(string), "@")[0]
-				cmd := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", dep)
-				cmd.Stdout = nil
-				if _, err := cmd.Output(); err != nil {
-					fmt.Printf("Could not install %s\n", []any{dep}...)
-					return 1, fmt.Errorf("could not install dependency %s", dep)
-				} else {
-					fmt.Printf("Installed %s\n", []any{dep}...)
-				}
-			} else {
-				dep := strings.Split(f.Deps[i].(string), "@")
-				cmd := exec.Command("python3.13", "-m", "pip", "install", dep[0], "==", dep[1])
-				cmd.Stdout = nil
-				if _, err := cmd.Output(); err != nil {
-					fmt.Printf("Could not install %s\n", []any{dep}...)
-					return 1, fmt.Errorf("could not install dependency %s", dep)
-				} else {
-					fmt.Printf("Installed %s\n", []any{dep[0]}...)
-				}
-			}
+		if _, err := installDeps(f.Deps, false); err != nil {
+			return 1, err
 		}
 		fmt.Println("Dependencies installed")
 		fmt.Print("Running build commands... ")
--- rpkgengine/rpkgengine.go
+++ rpkgengine/rpkgengine.go
@@ -1,5 +1,11 @@
 package rpkgengine
 
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
 // RpkgBuildFile is the struct for the rpkg.build.yaml file
 type RpkgBuildFile struct {
 	Name          string
@@ -28,4 +34,56 @@ func Hello() string {
 \\                \\              ..
 	`
 	return text
+}
+func installDeps(deps []interface{}, buildDeps bool) (int, error) {
+	// Install dependencies
+	for i := 0; i < len(deps); i++ {
+		if deps[0] == "none" {
+			if buildDeps {
+				fmt.Println("No build dependencies")
+				break
+			} else {
+				fmt.Println("No dependencies")
+				break
+			}
+		} else if dep, ok := deps[i].(string); ok && !strings.Contains(dep, "@") {
+			if buildDeps {
+				fmt.Printf("Build dependency %s is not a valid dependency\n", []any{dep}...)
+				return 1, fmt.Errorf("build dependency %s is not a valid dependency", dep)
+			} else {
+				fmt.Printf("Dependency %s is not a valid dependency\n", []any{dep}...)
+				return 1, fmt.Errorf("dependency %s is not a valid dependency", dep)
+			}
+		} else if dep, ok := deps[i].(string); ok && strings.Contains(dep, "@latest") {
+			fmt.Printf("Installing %s... ", []any{deps[i]}...)
+			dep := strings.Split(deps[i].(string), "@")[0]
+			cmd := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", dep)
+			cmd.Stdout = nil
+			if _, err := cmd.Output(); err != nil {
+				fmt.Printf("Could not install %s\n", []any{dep}...)
+				if buildDeps {
+					return 1, fmt.Errorf("could not install build dependency %s", dep)
+				} else {
+					return 1, fmt.Errorf("could not install dependency %s", dep)
+				}
+			} else {
+				fmt.Printf("Installed %s\n", []any{dep}...)
+			}
+		} else {
+			dep := strings.Split(deps[i].(string), "@")
+			cmd := exec.Command("python3.13", "-m", "pip", "install", dep[0], "==", dep[1])
+			cmd.Stdout = nil
+			if _, err := cmd.Output(); err != nil {
+				fmt.Printf("Could not install %s\n", []any{dep}...)
+				if buildDeps {
+					return 1, fmt.Errorf("could not install build dependency %s", dep)
+				} else {
+					return 1, fmt.Errorf("could not install dependency %s", dep)
+				}
+			} else {
+				fmt.Printf("Installed %s\n", []any{dep[0]}...)
+			}
+		}
+	}
+	return 0, nil
 }
