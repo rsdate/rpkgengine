1e9143500269bacd4977416f2a123c6c2990754c
Rohan Date <rohan.s.date@icloud.com>
2025-03-10 19:20:44 -0700

    Added advanced error handling

--- rpkgengine/build.go
+++ rpkgengine/build.go
@@ -6,6 +6,19 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	e "github.com/rsdate/utils/errors"
+	t "github.com/rsdate/utils/types"
+)
+
+var (
+	errChecker = e.ErrChecker{
+		ErrPrefix: "build error",
+		PanicMode: "true",
+		EM:        e.EM["eMre"],
+		TestMode:  false,
+	}
+	Em = errChecker.EM
 )
 
 // Description: Build builds the package
@@ -15,80 +28,80 @@ import (
 // Returns: It returns an integer and an error. The integer is the exit code of the build process (1 or 0) and the error is any error that occurred during the build process
 func Build(project string, f RpkgBuildFile, removeProjectFolder bool) (int, error) {
 	os.Chdir(project + "/Package")
-	wd, _ := os.Getwd()
+	wd := project + "/Package"
 	fmt.Printf("Building package in %v\n", wd)
 	switch lang := f.BuildWith; lang {
 	case "python3.13":
 		// Check if python3.13 is installed
 		fmt.Print("Scanning for Python... ")
-		cmd := exec.Command("python3.13", "-v")
-		cmd.Stdout = nil
-		if _, err := cmd.Output(); err != nil {
-			fmt.Print("Not Found")
-			return 1, errors.New("python 3.13 was not found on your system")
-		} else {
-			fmt.Print("Found version 3.13\n")
-		}
+		var val, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			cmd := exec.Command("python3", "--version")
+			return cmd.Output()
+		})
+		fmt.Printf("Found version %s\n", t.Cast(val, false, "string").(string)[7:])
 		// Upgrade pip
-		fmt.Println("Upgrading pip... ")
-		cmd2 := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", "pip")
-		cmd2.Stdout = nil
-		if _, err := cmd2.Output(); err != nil {
-			fmt.Println("Could not upgrade pip")
-			return 1, errors.New("could not upgrade pip")
-		} else {
-			fmt.Println("Pip upgraded successfully")
-		}
+		fmt.Print("Upgrading pip... ")
+		var _, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			cmd2 := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", "pip")
+			return cmd2.Output()
+		})
+		fmt.Println("Pip upgraded successfully")
 		// Install build dependencies
-		fmt.Println("Installing build dependencies... ")
-		if _, err := installDeps(f.BuildDeps, true); err != nil {
-			return 1, err
-		}
+		fmt.Print("Installing build dependencies... ")
+		var _, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			return nil, installPythonDeps(f.BuildDeps, true)
+		})
 		fmt.Println("Build dependencies installed")
 		// Install dependencies
-		fmt.Println("Installing dependencies... ")
-		if _, err := installDeps(f.Deps, false); err != nil {
-			return 1, err
-		}
+		fmt.Print("Installing dependencies... ")
+		var _, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			return nil, installPythonDeps(f.Deps, false)
+		})
 		fmt.Println("Dependencies installed")
 		// Run build commands
 		fmt.Print("Running build commands... ")
-		cmds := ""
-		for i := 0; i < len(f.BuildCommands); i++ {
-			if cmd, ok := f.BuildCommands[i].(string); ok {
-				cmds += cmd + " && "
-			} else {
-				fmt.Printf("Build command %v is not a string\n", []any{f.BuildCommands[i]}...)
-				return 1, fmt.Errorf("build command %v is not a string", f.BuildCommands[i])
+		var _, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			cmds := ""
+			for i := range f.BuildCommands {
+				if cmd, ok := f.BuildCommands[i].(string); ok {
+					cmds += cmd + " && "
+				} else {
+					fmt.Printf("Build command %v is not a string\n", []any{f.BuildCommands[i]}...)
+					return nil, fmt.Errorf("build command %v is not a string", []any{f.BuildCommands[i]}...)
+				}
 			}
-		}
-		cmds = cmds[:len(cmds)-4]
-		Cmd := exec.Command("sh", "-c", cmds)
-		Cmd.Stdout = nil
-		if _, err := Cmd.Output(); err != nil {
-			fmt.Println("Could not run build commands")
-			return 1, errors.New("build commands could not be run")
-		}
+			cmds = cmds[:len(cmds)-4]
+			Cmd := exec.Command("sh", "-c", cmds)
+			Cmd.Stdout = nil
+			if _, err := Cmd.Output(); err != nil {
+				fmt.Println("Could not run build commands")
+				return 1, errors.New("build commands could not be run")
+			}
+			return nil, nil
+		})
+		fmt.Println("Build commands ran successfully.")
+		// Clean up
+		fmt.Print("Cleaning up... ")
+		var _, _ = errChecker.CheckErr(Em[""], func() (any, error) {
+			cmd := exec.Command("mv", "./dist/", "../dist/")
+			cmd.Stdout = nil
+			if _, err := cmd.Output(); err != nil {
+				fmt.Println("Could not move dist folder.")
+				return 1, errors.New("could not move dist folder")
+			}
+			if removeProjectFolder {
+				fmt.Println("Removing project folder... ")
+				cmd2 := exec.Command("rm", "-rf", "./")
+				cmd2.Stdout = nil
+				if _, err := cmd2.Output(); err != nil {
+					fmt.Println("Could not remove project folder.")
+					return 1, errors.New("could not remove project folder")
+				}
+			}
+			return nil, nil
+		})
+		// Success (exit code 0)
 	}
-	fmt.Println("Build commands ran successfully.")
-	// Clean up
-	fmt.Println("Cleaning up... ")
-	cmd := exec.Command("mv", "./dist/", "../dist/")
-	cmd.Stdout = nil
-	if _, err := cmd.Output(); err != nil {
-		fmt.Println("Could not move dist folder.")
-		return 1, errors.New("could not move dist folder")
-	}
-	if removeProjectFolder {
-		fmt.Println("Removing project folder... ")
-		cmd2 := exec.Command("rm", "-rf", "./")
-		cmd2.Stdout = nil
-		if _, err := cmd2.Output(); err != nil {
-			fmt.Println("Could not remove project folder.")
-			return 1, errors.New("could not remove project folder")
-		}
-	}
-	// Success (exit code 0)
 	fmt.Println("Package built successfully.")
 	return 0, nil
 }
--- rpkgengine/rpkgengine.go
+++ rpkgengine/rpkgengine.go
@@ -8,38 +8,30 @@ import (
 )
 
 // RpkgBuildFile is the struct for the rpkg.build.yaml file
-//
-// Name: The name of the package (this should be the same as the name of the package folder) e.g. test
-//
-// Version: The version of the package (this should be in the format of major.minor.patch) e.g. 1.0.0
-//
-// Revision: The revision of the package (default is 0 for a new major, minor or patch version) e.g. 0fa5d3
-//
-// Authors: The authors of the package (this should be a list of authors) e.g. ["John Doe", "Jane Doe"]
-//
-// Deps: The dependencies of the package (this should be a list of dependencies) e.g. ["requests@latest", "flask@1.1.2"]
-//
-// BuildDeps: The build dependencies of the package (this should be a list of build dependencies) e.g. ["pytest@latest", "flake8@3.9.2"]
-//
-// BuildWith: The language the package is built with (this should be the language the package is built with with a version right after it) e.g. python3.13
-//
-// BuildCommands: The build commands of the package (this should be a list of build commands that build your package) e.g. ["python3.13 setup.py sdist"]
 type RpkgBuildFile struct {
-	Name          string
-	Version       string
-	Revision      int
-	Authors       []interface{}
-	Deps          []interface{}
-	BuildDeps     []interface{}
-	BuildWith     string
-	BuildCommands []interface{}
+	// Name: The name of the package (this should be the same as the name of the package folder) e.g. test
+	Name string
+	// Version: The version of the package (this should be in the format of major.minor.patch) e.g. 1.0.0
+	Version string
+	// Revision: The revision of the package (default is 0 for a new major, minor or patch version) e.g. 0fa5d3
+	Revision int
+	// Authors: The authors of the package (this should be a list of authors) e.g. ["John Doe", "Jane Doe"]
+	Authors []any
+	// Deps: The dependencies of the package (this should be a list of dependencies) e.g. ["requests@latest", "flask@1.1.2"]
+	Deps []any
+	// BuildDeps: The build dependencies of the package (this should be a list of build dependencies) e.g. ["pytest@latest", "flake8@3.9.2"]
+	BuildDeps []any
+	// BuildWith: The language the package is built with (this should be the language the package is built with with a version right after it) e.g. python3.13
+	BuildWith string
+	// BuildCommands: The build commands of the package (this should be a list of build commands that build your package) e.g. ["python3.13 setup.py sdist"]
+	BuildCommands []any
 }
 
-// Description: Hello returns a string of a text art
+// Description: Hello is a function to introduce rpkg
 //
 // Parameters: None
 //
-// Returns: It returns a string of a text art
+// Returns: It returns a string of a text art.
 func Hello() string {
 	text := `
 \\  \\  \\  \\  \\                  .. .. .. .. .. ..  
@@ -58,59 +50,62 @@ func Hello() string {
 	return text
 }
 
-// Description: installDeps installs dependencies. It is a helper function for the Build function (also so that it wasn't too long)
+// Description: installDeps installs dependencies. It is a helper function for the Build function (also so that it wasn't too long).
 //
-// Parameters: It takes a list of dependencies and a boolean to check if the dependencies are build dependencies
+// Parameters: It takes a list of dependencies and a boolean to check if the dependencies are build dependencies.
 //
-// Returns: It returns an integer and an error. The integer is the exit code of the dependency installation process (1 or 0) and the error is any error that occurred during the dependency installation process
-func installDeps(deps []interface{}, buildDeps bool) (int, error) {
-	for i := 0; i < len(deps); i++ {
-		if deps[0] == "none" {
-			if buildDeps {
-				fmt.Println("No build dependencies")
-				break
-			} else {
-				fmt.Println("No dependencies")
-				break
-			}
-		} else if dep, ok := deps[i].(string); ok && !strings.Contains(dep, "@") {
-			if buildDeps {
-				fmt.Printf("Build dependency %s is not a valid dependency\n", []any{dep}...)
-				return 1, fmt.Errorf("build dependency %s is not a valid dependency", dep)
-			} else {
-				fmt.Printf("Dependency %s is not a valid dependency\n", []any{dep}...)
-				return 1, fmt.Errorf("dependency %s is not a valid dependency", dep)
-			}
-		} else if dep, ok := deps[i].(string); ok && strings.Contains(dep, "@latest") {
-			fmt.Printf("Installing %s... ", []any{deps[i]}...)
-			dep := strings.Split(deps[i].(string), "@")[0]
-			cmd := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", dep)
-			cmd.Stdout = nil
-			if _, err := cmd.Output(); err != nil {
-				fmt.Printf("Could not install %s\n", []any{dep}...)
+// Returns: It returns an error. The error is any error that occurred during the dependency installation process.
+func installPythonDeps(deps []any, buildDeps bool) error {
+	var _, err = errChecker.CheckErr(Em[""], func() (any, error) {
+		for i := range deps {
+			if deps[0] == "none" {
 				if buildDeps {
-					return 1, fmt.Errorf("could not install build dependency %s", dep)
+					fmt.Println("No build dependencies")
+					break
 				} else {
-					return 1, fmt.Errorf("could not install dependency %s", dep)
+					fmt.Println("No dependencies")
+					break
+				}
+			} else if dep, ok := deps[i].(string); ok && !strings.Contains(dep, "@") {
+				if buildDeps {
+					fmt.Printf("Build dependency %s is not a valid dependency\n", []any{dep}...)
+					return nil, fmt.Errorf("build dependency %s is not a valid dependency", dep)
+				} else {
+					fmt.Printf("Dependency %s is not a valid dependency\n", []any{dep}...)
+					return nil, fmt.Errorf("dependency %s is not a valid dependency", dep)
+				}
+			} else if dep, ok := deps[i].(string); ok && strings.Contains(dep, "@latest") {
+				fmt.Printf("Installing %s... ", []any{deps[i]}...)
+				dep := strings.Split(deps[i].(string), "@")[0]
+				cmd := exec.Command("python3.13", "-m", "pip", "install", "--upgrade", dep)
+				cmd.Stdout = nil
+				if _, err := cmd.Output(); err != nil {
+					fmt.Printf("Could not install %s\n", []any{dep}...)
+					if buildDeps {
+						return nil, fmt.Errorf("could not install build dependency %s", dep)
+					} else {
+						return nil, fmt.Errorf("could not install dependency %s", dep)
+					}
+				} else {
+					fmt.Printf("Installed %s\n", []any{dep}...)
 				}
 			} else {
-				fmt.Printf("Installed %s\n", []any{dep}...)
-			}
-		} else {
-			dep := strings.Split(deps[i].(string), "@")
-			cmd := exec.Command("python3.13", "-m", "pip", "install", dep[0], "==", dep[1])
-			cmd.Stdout = nil
-			if _, err := cmd.Output(); err != nil {
-				fmt.Printf("Could not install %s\n", []any{dep}...)
-				if buildDeps {
-					return 1, fmt.Errorf("could not install build dependency %s", dep)
+				dep := strings.Split(deps[i].(string), "@")
+				cmd := exec.Command("python3.13", "-m", "pip", "install", dep[0], "==", dep[1])
+				cmd.Stdout = nil
+				if _, err := cmd.Output(); err != nil {
+					fmt.Printf("Could not install %s\n", []any{dep}...)
+					if buildDeps {
+						return nil, fmt.Errorf("could not install build dependency %s", dep)
+					} else {
+						return nil, fmt.Errorf("could not install dependency %s", dep)
+					}
 				} else {
-					return 1, fmt.Errorf("could not install dependency %s", dep)
+					fmt.Printf("Installed %s\n", []any{dep[0]}...)
 				}
-			} else {
-				fmt.Printf("Installed %s\n", []any{dep[0]}...)
 			}
 		}
-	}
-	return 0, nil
+		return nil, nil
+	})
+	return err
 }
